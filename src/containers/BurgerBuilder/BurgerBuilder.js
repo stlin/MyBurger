a6bb2b755d1ca49c5d527aa49fecf492778fa1cf
Steve Lin <steve-lin@live.com>
2020-06-23 09:30:20 -0700

    Added current price display

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -72,7 +72,7 @@ class BurgerBuilder extends Component {
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler} 
                     ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo} />
+                    disabled={disabledInfo} price={this.state.totalPrice} />
             </Aux>
         );
     }
