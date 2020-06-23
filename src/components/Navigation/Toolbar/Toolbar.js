3c8dd60817c6c1f8ff85e84b4539ab26d9a7a3bf
Steve Lin <steve-lin@live.com>
2020-06-23 14:57:43 -0700

    Added sidedrawer but commented out due to issues

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,7 +7,9 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = (props) => (
     <header className="Toolbar">
         <div>MENU</div>
-        <Logo />
+        <div className="Logo">
+            <Logo />
+        </div>
         <nav>
             <NavigationItems />
         </nav>
