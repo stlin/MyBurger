04be12bcb7b250f99ade5aa872e00dbc7e88568f
Steve Lin <steve-lin@live.com>
2020-06-23 14:19:21 -0700

    Implemented navigation items

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 
 const toolbar = (props) => (
@@ -8,9 +9,8 @@ const toolbar = (props) => (
         <div>MENU</div>
         <Logo />
         <nav>
-            ...
+            <NavigationItems />
         </nav>
-
     </header>
 );
 
